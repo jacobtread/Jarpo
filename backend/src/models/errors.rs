8f6385c434212c449d0f45303d1947ff764c94a5
Jacobtread <jacobtread@gmail.com>
2022-09-22 10:42:55 +1200

    From implementations

--- backend/src/models/errors.rs
+++ backend/src/models/errors.rs
@@ -5,7 +5,6 @@ use actix_web::ResponseError;
 #[derive(Debug)]
 pub enum VersionsError {
     IO(io::Error),
-    Deserialization,
     Request(reqwest::Error),
 }
 
@@ -13,10 +12,21 @@ impl Display for VersionsError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             VersionsError::IO(err) => f.write_str(&format!("IO Error: {}", err)),
-            VersionsError::Deserialization => f.write_str("Deserialization error"),
             VersionsError::Request(err) => f.write_str(&format!("Request error: {}", err))
         }
     }
 }
 
 impl ResponseError for VersionsError {}
+
+impl From<reqwest::Error> for VersionsError {
+    fn from(err: reqwest::Error) -> Self {
+        VersionsError::Request(err)
+    }
+}
+
+impl From<io::Error> for VersionsError {
+    fn from(err: io::Error) -> Self {
+        VersionsError::IO(err)
+    }
+}
